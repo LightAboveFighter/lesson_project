7bff86a6bbcb61756be10c5620cbdd38189cfb3a
Alexander Kosarigin <skosarigin@gmail.com>
2023-09-16 13:22:48 +0300

    add progress Lesson 1

---
+++ constant_user.py
@@ -0,0 +1,13 @@
+from myconstants.math_constants import pi
+from myconstants.physical_constants import gravity_acc
+from myconstants.math_constants import sqrt2
+
+print(f"pi = {pi}")
+print(f"g = {gravity_acc}")
+print(f"sqrt2 = {sqrt2}")
+print(f"pi^0.5 = {pi**0.5}")
+from myconstants.chemical_const import O_mass
+
+print(f"pi = {pi}")
+print(f"g = {gravity_acc}")
+print(f"O_mass = {O_mass}")
